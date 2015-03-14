eb3d88aa5fb54e1f9d414f9cbedff5450bbe4e6f
fotcorn <fotcorn@gmail.com>
2015-03-14 18:16:54 +0100

    added material-ui

--- client/js/components.js
+++ client/js/components.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
+import {FlatButton} from 'material-ui';
 
 export var Test = React.createClass({
     render() {
-        return <a onClick={this.eventHandler}>Hello World62</a>
+        return <div>
+            <FlatButton label="Default" />
+            <a onClick={this.eventHandler}>Hello World62</a>
+        </div>
     },
     eventHandler() {
         alert('handler!');
