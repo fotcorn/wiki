7c3bd90e54abddac0de14668af6b70ca548bbafa
fotcorn <fotcorn@gmail.com>
2015-03-14 18:18:12 +0100

    added react-tap-event-plugin plugin code to main.js

--- client/js/main.js
+++ client/js/main.js
@@ -1,5 +1,8 @@
 import {Test} from './components.js';
 import React from 'react';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+injectTapEventPlugin();
 
 React.render(
     <Test />,
