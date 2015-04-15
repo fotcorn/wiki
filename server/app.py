fe5991d2f8281d313b89003cf19da88b3cdea53f
fotcorn <fotcorn@gmail.com>
2015-04-15 10:13:44 +0200

    server: use redis store to fetch pages

--- server/app.py
+++ server/app.py
@@ -1,13 +1,18 @@
 from flask import Flask, jsonify
 from flask.ext.cors import CORS
+from redis_store import RedisStore
 
 
 app = Flask(__name__)
 cors = CORS(app)
 
+r = RedisStore()
+
+USERNAME = 'admin'  # hardcoded for now
+
 @app.route('/api/pages/<page>/')
 def page(page):
-    return jsonify({'text': u'# {}'.format(page)})
+    return jsonify({'text': r.get_page(USERNAME, page)})
 
 if __name__ == '__main__':
     app.run(debug=True)
