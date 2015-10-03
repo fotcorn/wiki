7f97dcbb8c2abc729f2f5f7fa196075cd1719b14
fotcorn <fotcorn@gmail.com>
2015-10-03 13:16:21 +0200

    [Server] api: bugfixes

--- server/api/views.py
+++ server/api/views.py
@@ -1,17 +1,19 @@
 import json
 from django.http.response import JsonResponse, HttpResponseForbidden
+from django.views.decorators.csrf import csrf_exempt
 from .redis_store import RedisStore
 
 r = RedisStore()
 
 
+@csrf_exempt
 def page(request, page_name):
     if not request.user.is_authenticated():
         return HttpResponseForbidden()
 
     if request.method == 'POST':
-        data = json.loads(request.data)
-        r.set_page(request.user.username, page_name, data['text'])
+        data = json.loads(request.body)
+        r.set_page(request.user.email, page_name, data['text'])
         return JsonResponse({'status': 'ok'})
     else:
-        return JsonResponse({'text': r.get_page(request.user.username, page_name)})
+        return JsonResponse({'text': r.get_page(request.user.email, page_name)})
