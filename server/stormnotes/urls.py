66a91b1c9f61b7b8ce2b75bb05843c369099046d
fotcorn <fotcorn@gmail.com>
2015-10-03 12:35:20 +0200

    [Server] removed django admin for now

--- server/stormnotes/urls.py
+++ server/stormnotes/urls.py
@@ -1,8 +1,6 @@
 from django.conf.urls import include, url
-from django.contrib import admin
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
     url(r'^api/', include('api.urls', namespace='api')),
     url(r'^account/', include('account.urls', namespace='account')),
 ]
