53cd7d56590af550a056604639348178ad6a7355
fotcorn <fotcorn@gmail.com>
2015-10-11 16:41:24 +0200

    [Auth] added logout view

--- server/account/urls.py
+++ server/account/urls.py
@@ -3,4 +3,5 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^login/$', auth_views.login, name='login'),
+    url(r'^logout/$', auth_views.logout, name='logout', kwargs={'next_page': 'account:login'}),
 ]
