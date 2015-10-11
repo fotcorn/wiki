f9fb47ca137e9318a847b459fed407f2a2aeb417
fotcorn <fotcorn@gmail.com>
2015-10-11 17:11:45 +0200

    [Dev] fixed roboto_fonts not found error

--- server/stormnotes/urls.py
+++ server/stormnotes/urls.py
@@ -13,6 +13,9 @@ from django.views import static
 if settings.SYSTEM == 'development':
     urlpatterns += [
         url(r'^wiki/', views.index),
+        url(r'^static/fonts/(?P<path>.*)$', static.serve, kwargs={
+            'document_root': os.path.join(settings.BASE_DIR, '..', 'client', 'node_modules', 'roboto-font', 'fonts')
+        }),
         url(r'^static/(?P<path>.*)$', static.serve, kwargs={
             'document_root': os.path.join(settings.BASE_DIR, '..', 'client')
         }),
