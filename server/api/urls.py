4f93911fbd41773c1bca18656f2dcf4adbcf925c
fotcorn <fotcorn@gmail.com>
2015-10-24 22:38:35 +0200

    [Server] urls: fixed page names with underline

--- server/api/urls.py
+++ server/api/urls.py
@@ -2,5 +2,5 @@ from . import views
 from django.conf.urls import url
 
 urlpatterns = [
-    url(r'^pages/(?P<page_name>[a-zA-Z0-9:]+)/$', views.page, name='page')
+    url(r'^pages/(?P<page_name>[a-zA-Z0-9\-_:]+)/$', views.page, name='page')
 ]
